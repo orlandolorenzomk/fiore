34506e853fb52869916892bfb3914672dee89b5a
lorenzo <orlandolorenzo@kreyzon.com>
2026-02-20 16:42:25 +0200

    fuck it

--- jpack/include/command_dispatcher.h
+++
@@ -1,8 +0,0 @@
-#ifndef COMMAND_DISPATCHER_H
-#define COMMAND_DISPATCHER_H
-
-#include <stdbool.h>
-
-void dispatch(const char *command, const char *package, const char *version, bool list_remote);
-
-#endif // COMMAND_DISPATCHER_H
